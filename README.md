5bc86893d20240998de04174c862aae8adac2f6f
Keir Badger <keir.badger@acuris.com>
2021-01-08 15:01:01 +0000

    add code in

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ion-suffix-generator
+
+This repo is used by the ION components to assign a unique suffix 
---
+++ give_me_a_suffix.sh
@@ -0,0 +1,41 @@
+task_id=$(grep TaskARN $ECS_CONTAINER_METADATA_FILE | awk -F'"' '{print $4}')
+
+while true
+do
+    i=0
+    while [ $i -lt $DESIRED_COUNT ]
+    do
+        let suffix=$START_SUFFIX+$i
+        row=$(aws dynamodb get-item --table-name ${SUFFIX_TABLE} --key "{\"suffix\": {\"S\": \"$suffix\"}}")
+        echo $task_id ":" $suffix ":" $row
+
+        if [ -z "${row}" ] ; then
+            aws dynamodb put-item \
+              --table-name ${SUFFIX_TABLE} \
+              --item "{\"suffix\": {\"S\": \"${suffix}\"},\"taskid\": {\"S\": \"${task_id}\"}}" \
+              --condition-expression "attribute_not_exists(taskid)" \
+              --region eu-west-1
+            if [ $? -eq 0 ] ; then
+                echo "${task_id}:GOT SUFFIX '${suffix}', adding new row"
+                break 2
+            fi
+        else
+            saved_task_id=$(echo ${row} | jq '.Item.taskid.S' | sed 's/"//g')
+        fi
+
+        all_tasks=$(aws ecs list-tasks --service-name  ${ENV_NAME}-ion-xtract-router --region eu-west-1)
+        if ! echo "${all_tasks}" | grep -q ${saved_task_id} ; then
+            aws dynamodb put-item \
+              --table-name ${SUFFIX_TABLE} \
+              --item "{\"suffix\": {\"S\": \"${suffix}\"},\"taskid\": {\"S\": \"${task_id}\"}}" \
+              --condition-expression "taskid = :t" \
+              --expression-attribute-values "{\":t\": {\"S\": \"${saved_task_id}\"}}" \
+              --region eu-west-1
+            if [ $? -eq 0 ] ; then
+                echo "${task_id}:GOT SUFFIX '${suffix}', replacing old id '${saved_task_id}'"
+                break 2
+            fi
+        fi
+        let i=$i+1
+    done
+done
